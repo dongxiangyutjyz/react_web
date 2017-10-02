5896d2ba895a7155d10513bb28b2a999347467a4
James <xdong2@nd.edu>
2017-10-01 22:10:40 -0400

    Client side updated

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+const Landing = () => {
+  return (
+    <div>
+      <h1>
+        About Us
+      </h1>
+    </div>
+  );
+};
+
+export default Landing;
